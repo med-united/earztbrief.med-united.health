d2426b2a66d1887c6a1391ca45c25f28559ebade
simoneS96 <simone.stifano@incentergy.de>
2022-05-04 15:38:26 +0200

    MEDU-65: create-button-receiver-email

--- earztbrief/controller/InputForm.controller.js
+++ earztbrief/controller/InputForm.controller.js
@@ -2,14 +2,14 @@ sap.ui.define(
     ["sap/ui/core/mvc/Controller", "sap/ui/model/xml/XMLModel"],
     function (Controller, XMLModel) {
         "use strict";
-        
+
         return Controller.extend("health.med-united.earztbrief", {
             onInit: function () {
                 this.getView().setModel(
                     new XMLModel("./template/Arztbrief-Minimal.XML")
                 );
             },
-            onClick: function() {
+            onClick: function () {
                 const oXmlModel = this.getView().getModel();
                 const oXmlDoc = oXmlModel.getData();
                 const sXml = new XMLSerializer().serializeToString(oXmlDoc.documentElement);
@@ -18,20 +18,20 @@ sap.ui.define(
                 // Send email via backend
                 const templateParams = {
                     contactname: oXmlModel.getProperty("/recordTarget/patientRole/patient/name/given")
-                                +" "
-                                +oXmlModel.getProperty("/recordTarget/patientRole/patient/name/family"),
-                    contactemail: "istvan.orosz@incentergy.de",
+                        + " "
+                        + oXmlModel.getProperty("/recordTarget/patientRole/patient/name/family"),
+                    contactemail: oXmlModel.getProperty("/recordTarget/patientRole/providerOrganization/telecom/@value"),
                     contactmessage: oXmlModel.getProperty("/component/structuredBody/component/section").toString(),
                     attachment: sXml,
                 };
                 fetch('/sendEmail', {
                     method: 'POST',
                     headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(templateParams)
-                  });
+                });
             }
         });
     }
