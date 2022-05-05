3fcca47b228cf7ae3e9f45970b29a7385f7605c8
ruszkipista <oi5672@gmail.com>
2022-05-05 21:41:11 +0100

    refactor the bootstrap mechanism

---
+++ webapp/Component.js
@@ -0,0 +1,18 @@
+sap.ui.define(
+    ["sap/ui/core/UIComponent",
+     "sap/ui/model/json/JSONModel"
+    ],
+    function (UIComponent, JSONModel) {
+        "use strict";
+        return UIComponent.extend("health.med-united.earztbrief.Component", {
+
+            metadata : {
+                interfaces: ["sap.ui.core.IAsyncContentCreation"],
+                manifest: "json"
+            },
+
+            init : function () {
+                UIComponent.prototype.init.apply(this, arguments);
+            }
+        });
+ });
